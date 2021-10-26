1aa59ad524da124b3650e9ef91f6b654c3b1e384
SowmyaGonepally <sowmya.gonepally@zemosolabs.com>
2021-10-26 17:03:56 +0530

    Java Assignment-5 submitted

---
+++ JavaAssignment5/src/sowmya/assignment/singleton/Singleton.java
@@ -0,0 +1,25 @@
+package sowmya.assignment.singleton;
+
+public class Singleton {
+     String single;
+
+    public static Singleton initializeSingle(String single)
+    {
+        Singleton singleton=new Singleton();
+        /*
+
+        this.single=single;
+
+        */
+
+        //The keyword 'this' refers to instance of a class. It is a non-static keyword.
+         //Non-static variables cannot be referenced from a static block or method.
+         //Removing static keyword for the method initializeSingle will work.
+
+        return singleton;
+    }
+    public void printSingle()
+    {
+        System.out.println(single);
+    }
+}
